25b8343faa600be17059b1e2b6e0c94fc20c1d97
naveen-krv <80533551+naveen-krv@users.noreply.github.com>
2021-04-26 20:03:55 +0530

    Added String extension for empty validation

---
+++ my-extensions/my-extensions/Extensions/Validation+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  Validation+Extension.swift
+//  my-extensions
+//
+//  Created by Naveen Kumar V on 26/04/21.
+//
+
+import Foundation
---
+++ my-extensions/my-extensions/my_extensions.h
@@ -0,0 +1,18 @@
+//
+//  my_extensions.h
+//  my-extensions
+//
+//  Created by Naveen Kumar V on 26/04/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for my_extensions.
+FOUNDATION_EXPORT double my_extensionsVersionNumber;
+
+//! Project version string for my_extensions.
+FOUNDATION_EXPORT const unsigned char my_extensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <my_extensions/PublicHeader.h>
+
+
